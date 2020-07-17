73947428b0ca248dfb1a58f085094ddc0ad75025
かみざとよしと <kamizatoyoshito@kamizatoyoshitonoMacBook-Air.local>
2020-07-17 18:29:03 +0900

    generatorの設定変更（rails gコマンドでassets、helperが作成されないように設定） homes画面に各種リンク（仮）を設定

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module YanbaruSpace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false  
+      g.helper false
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+    end
   end
 end
