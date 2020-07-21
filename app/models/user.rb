4a6dfd9dee2771de5332c2c7d54c67801433fdcc
Seidai Inoue <shengdajingshang@gmail.com>
2020-07-21 23:30:48 +0900

    正規表現の語尾修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :omniauthable,:validatable,
-         omniauth_providers: %i[google_oauth2] #複数追加したいとき -> %i[facebook twitter google_oauth2]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2] #複数追加したいとき -> %i[facebook twitter google_oauth2]
 
-  # validates :email, presence: { message: 'が入力されていません。' }, uniqueness: true
-  # validates :password, presence: { message: 'が入力されていません。' }, confirmation: true
   validate :password_complexity
 
   def password_complexity
     # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])$/
 
-    errors.add(:password, '：1文字以上の大文字、小文字、記号を使用してください')
+    errors.add :password, '：1文字以上の大文字、小文字、記号を使用し全部で8文字以上にしてください'
   end
-
-
 
   # omniauthのコールバック時に呼ばれるメソッド
   def self.from_omniauth(auth)
