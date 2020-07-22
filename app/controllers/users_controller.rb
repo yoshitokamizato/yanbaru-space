cae18e2a10ad8af33df9606e121dbfb4c7502e52
shibatani <rto1728@gmail.com>
2020-07-22 22:36:03 +0900

    ユーザーのマイページを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email)
+  end
+end
