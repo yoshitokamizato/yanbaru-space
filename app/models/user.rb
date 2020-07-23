8a8ff58f6566974c4ae462aef04c07309056333d
かみざとよしと <kamizatoyoshito@kamizatoyoshitonoMacBook-Air.local>
2020-07-23 10:17:27 +0900

    Heroku上でGoogleログイン時にrollbackが起こるためgoogleの画像はsessionに格納して使用 - google_imageのマイグレーション ファイル削除、テーブルからもgoogle_imageのカラム は削除 - コントローラ側でgoogle_imageに関するsessionの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[google_oauth2] #複数追加したいとき -> %i[facebook twitter google_oauth2]
 
   validate :password_complexity
-  validates :image, presence: true
 
   mount_uploader :image, ImageUploader
 
@@ -20,8 +19,7 @@ class User < ApplicationRecord
   # omniauthのコールバック時に呼ばれるメソッド
   def self.from_omniauth(auth, controller_name)
     @controller_name = controller_name
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.google_image = auth.info.image
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[8,20]
     end
