c0abd78ab1f761445615d93c0b7f17e5f38caa73
かみざとよしと <kamizatoyoshito@kamizatoyoshitonoMacBook-Air.local>
2020-07-25 15:00:33 +0900

    電話番号の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_094734) do
+ActiveRecord::Schema.define(version: 2020_07_24_065116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_094734) do
     t.string "provider"
     t.string "uid"
     t.string "image"
+    t.string "nickname"
     t.string "google_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
