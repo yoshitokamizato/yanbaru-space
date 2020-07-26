ea04b0faa6ca994e3e76bb78a232c238dfd2f76c
Seidai Inoue <shengdajingshang@gmail.com>
2020-07-26 14:02:53 +0900

    一覧画面のユーザもっと見るを変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,12 +2,6 @@ class HomesController < ApplicationController
   def index
     @topics = Topic.all
     @user = User.find(current_user.id)
-    @users = User.all.page(params[:page]).per(5)
-
-   return unless request.xhr?
-   case params[:type]
-   when 'user'
-      render "#{params[:type]}"
-   end
+    @users = User.first(5)
   end
 end
