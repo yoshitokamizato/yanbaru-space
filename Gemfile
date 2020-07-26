7b743ab5aad7328db5c847d6d02e33aa7ab89c00
Seidai Inoue <shengdajingshang@gmail.com>
2020-07-26 12:21:59 +0900

    ユーザー一覧をトップに表示

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'activeadmin'
 # 画像投稿
 gem 'carrierwave'
 
+# ページネーション
+gem 'kaminari'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,13 @@
 class HomesController < ApplicationController
   def index
     @topics = Topic.all
-
     @user = User.find(current_user.id)
+    @users = User.all.page(params[:page]).per(5)
+
+   return unless request.xhr?
+   case params[:type]
+   when 'user'
+      render "#{params[:type]}"
+   end
   end
 end
