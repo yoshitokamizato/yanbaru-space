d277b5d74d64ecf681bbd6e9631fdb66ff1ecf60
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-07-30 11:16:56 +0900

    マイグレーションを実行して更新#59

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
   def index
     @topics = Topic.all
     @topics = Topic.first(5)
-    # @user = User.find(current_user.id)
+    @user = User.find(current_user.id)
     @users = User.first(5)
     @menus = Menu.first(5)
 
