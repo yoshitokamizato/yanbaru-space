2584e41acc716deb3abf507270ce049374225db4
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-08-01 18:23:55 +0900

    home画面と一覧画面の表示順番を新規追加順に変更#71

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
     @topics = Topic.first(5)
     @users = User.first(5)
     @menus = Menu.first(5)
-    @events = Event.first(5)
+    reverse_event = Event.all.reverse
+    @events = reverse_event.first(5)
   end
 end
