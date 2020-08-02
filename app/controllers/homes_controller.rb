8680b2b49baf52109a098efec85219197a33ae9a
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-08-02 17:42:41 +0900

    homesコントローラを修正#71

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,6 @@ class HomesController < ApplicationController
     @topics = Topic.first(5)
     @users = User.first(5)
     @menus = Menu.first(5)
-    reverse_event = Event.all.reverse
-    @events = reverse_event.first(5)
+    @events = Event.last(5).reverse
   end
 end
