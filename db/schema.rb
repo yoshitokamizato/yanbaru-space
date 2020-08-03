db7c6cfc990f5720066848a38ac0ce498b6e3819
shibatani <rto1728@gmail.com>
2020-08-03 10:41:21 +0900

    フリーランス一覧も部分テンプレートを使用

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2020_07_31_005847) do
     t.string "uid"
     t.string "image"
     t.string "nickname"
-    t.string "google_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
