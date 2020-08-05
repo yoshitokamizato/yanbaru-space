6b602db4e067480c4c2103a3c3beabf2038e9a98
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-08-05 15:50:27 +0900

    モデルの作成#85

---
+++ app/models/contact_mail.rb
@@ -0,0 +1,2 @@
+class ContactMail < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_005847) do
+ActiveRecord::Schema.define(version: 2020_08_05_064626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2020_07_31_005847) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "contact_mails", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "subject"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "events", force: :cascade do |t|
@@ -76,7 +85,6 @@ ActiveRecord::Schema.define(version: 2020_07_31_005847) do
     t.string "uid"
     t.string "image"
     t.string "nickname"
-    t.string "google_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
