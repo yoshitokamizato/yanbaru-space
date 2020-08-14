54c8cd746607fa060d213b93cb91b678081b35db
shibatani <rto1728@gmail.com>
2020-08-14 22:22:41 +0900

    チェックボックスの記述変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,18 +18,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
 
-    users_skills = UserSkill.where(user_id: current_user.id).pluck(:skill_id)
-    selected_skill_ids = params[:user][:id].map!(&:to_i)
-
-    selected_skill_ids.each do |skill_id|
-      unless users_skills.include?(skill_id)
-        UserSkill.create(user_id: current_user.id, skill_id: skill_id)
-      end
-    end
-
-    delete_skills = users_skills - selected_skill_ids
-    UserSkill.where(skill_id: delete_skills).destroy_all
-
     if @user.update(user_params)
       redirect_to user_path(current_user), notice: "更新しました。"
     else
@@ -40,6 +28,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :image, :nickname, :id)
+    params.require(:user).permit(:email, :image, :nickname, :id, {  skill_ids: [] })
   end
 end
